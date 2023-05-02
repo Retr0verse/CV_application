3a19ebebf9a9f6ebca23e386869047a596c3c32a
Jonathan Kennedy <gt4magnet@protonmail.com>
2023-05-02 13:41:43 -0500

    add setstates to contact portion of form

--- cv-project/src/components/contact.js
+++ cv-project/src/components/contact.js
@@ -4,16 +4,42 @@ class Contact extends Component {
     constructor() {
         super();
 
-        this.state= {
+        this.state = {
             Contact: '',
             Contact: [
-              email: '',
-              phone: '',
-              city: '',
-              LinkedIn: '',
-              Github: '',  
+                email: '',
+                phone: '',
+                city: '',
+                LinkedIn: '',
+                Github: '',
+                edit: false
 
             ]
         }
-}
-}
+    }
+
+    handleEmailChange = e => {
+        this.setState({
+            email: e.target.value
+        })
+    }
+    handlePhoneChange = e => {
+        this.setState({
+            phone: e.target.value
+        })
+    }
+    handleCityChange = e => {
+        this.setState({
+            email: e.target.value
+        })
+    }
+    handleLinkedInChange = e => {
+        this.setState({
+            linkedIn: e.target.value
+        })
+    }
+    handleGithubChange = e => {
+        this.setState({
+            github: e.target.value
+        })
+    }
