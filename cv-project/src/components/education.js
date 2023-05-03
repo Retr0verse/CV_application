ac4744e79de437d0faee0fe45dc357347dc6f78b
Jonathan Kennedy <gt4magnet@protonmail.com>
2023-05-03 14:38:52 -0500

    add first two section of education component, label fields and placeholders

--- cv-project/src/components/education.js
+++ cv-project/src/components/education.js
@@ -6,5 +6,30 @@ class Education extends Component {
         super();
 
 
-        this.state 
+        this.state = {
+            degreeType: '',
+            major: '',
+            school: '',
+            startDate: '',
+            endDate: '',
+            location: '',
+            education: [
+                {
+                    id: uniqid(),
+                    degreetype: 'Bachelors of Science',
+                    major: 'Software Development',
+                    school: 'Rivercity University',
+                    date: 'Sept 2020 - May 2022',
+                    location: 'San Antonio, TX',
+
+
+                }
+            ],
+            edit: false
+        }
     }
+
+    handleDegreeChange = e => {
+        this.setState({
+            degreeType: e.target.value
+        })
