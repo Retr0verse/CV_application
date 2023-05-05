777eef2992fe05bdc234179296fe88748a4a71e5
Jonathan Kennedy <gt4magnet@protonmail.com>
2023-05-05 13:43:53 -0500

    add sections and import for CV section

--- cv-project/src/components/CV.js
+++ cv-project/src/components/CV.js
@@ -1,4 +1,18 @@
 import React, { Component } from "react";
+import Title from './Title';
 import Leftside from './Leftside';
 import Rightside from './Rightside';
 
+class CV extends Component {
+    render() {
+        return (
+            <div id="CV">
+                <Title />
+                <Leftside />
+                <Rightside />
+            </div>
+        )
+    }
+}
+
+export default CV;
--- cv-project/src/components/experience.js
+++ cv-project/src/components/experience.js
@@ -1,1 +1,7 @@
 import React, { Component } from 'react';
+
+
+
+
+
+export default Experience;
