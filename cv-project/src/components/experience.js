578fc7437f1e4f0c553a685f59a6df782b96e8d3
Jonathan Kennedy <gt4magnet@protonmail.com>
2023-05-05 16:22:52 -0500

    start experience section and key items

--- cv-project/src/components/experience.js
+++ cv-project/src/components/experience.js
@@ -1,4 +1,18 @@
 import React, { Component } from 'react';
+import uniqid from 'uniqid';
+
+class Experience extends Component {
+    constructor() {
+        super();
+
+        this.state = {
+            role: '',
+            company: '',
+            startDate: '',
+            endDate: '',
+            location: '',
+            details: '',
+    }
 
 
 
