54bfc908b8624ea7b97e3220e1a1ac82526e27ce
Jonathan Kennedy <gt4magnet@protonmail.com>
2023-05-08 14:50:25 -0500

    implement toggle functions experience section

--- cv-project/src/components/experience.js
+++ cv-project/src/components/experience.js
@@ -17,21 +17,95 @@ class Experience extends Component {
                 {
                     id: uniqid(),
                     role: 'Software Developer',
-                    company: 'Nudle',
+                    company: 'Broca Tech',
                     date: 'June 2021 - Current',
-                    location: '/ Los Angeles, CA',
+                    location: '/ London, UK',
+                    /*add four bulleted sections for experience details per job*/
                     details: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Molestie nunc non blandit massa enim nec dui.'
 
                 },
                 {
                     id: uniqid(),
                     role: 'Junior Web Developer',
-                    company: 'Blume',
-                    date: 'September 2019 - June 2021'
+                    company: 'Nudle',
+                    date: 'September 2019 - June 2021',
+                    location: '/ Palo Alto, CA',
+                    details: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Arcu vitae elementum curabitur vitae.'
+
+                },
+                {
+                    id: uniqid(),
+                    role: 'Web Design Intern',
+                    company: 'Nudle',
+                    date: 'April 2019 - September 2019',
+                    location: '/ Palo Alto, CA',
+                    details: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. At lectus urna duis convallis convallis tellus id interdum.'
+
+
                 }
-            ]
+            ],
+            edit: false
+        };
     }
 
+    handleRoleChange = e => {
+        this.setState({
+            role: e.target.value
+        })
+    }
+
+    handleCompanyChange = e => {
+        this.setState({
+            company: e.target.value
+        })
+    }
+
+    handleStartDateChange = e => {
+        this.setState({
+            startDate: e.target.value
+        })
+    }
+
+    handleEndDateChange = e => {
+        this.setState({
+            endDate: e.target.value
+        })
+    }
+
+    handleLocationChange = e => {
+        this.setState({
+            location: e.target.value
+        })
+
+        handleDetailsChange = e => {
+            this.setState({
+                details: e.target.value
+            })
+        }
+
+        onSubmitExperience = e => {
+            e.preventDefault();
+            this.setState({
+                experience: this.state.experience.concat({
+                    id: uniqid(),
+                    role: this.state.role,
+                    company: this.state.company,
+                    date: this.state.startDate + ' - ' + this.state.endDate,
+                    location: this.state.location,
+                    details: this.state.details
+
+                }),
+                role: '',
+                company: '',
+                startDate: '',
+                endDate: '',
+                location: '',
+                details: ''
+
+            })
+
+            this.toggleEdit()
+        }
 
 
 
