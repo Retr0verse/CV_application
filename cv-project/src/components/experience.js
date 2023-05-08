b843a6e011b6a0fa43dc5e6d417862e155134671
Jonathan Kennedy <gt4magnet@protonmail.com>
2023-05-08 17:24:11 -0500

    finished handle edit objects page complete besides minor syntax errors

--- cv-project/src/components/experience.js
+++ cv-project/src/components/experience.js
@@ -135,7 +135,95 @@ class Experience extends Component {
         
 
             return (
+                <div id="Experience">
+                    <h2>Work Experience</h2>
+                    {experience.map(exp => {
+                        return(
+                            <div key={exp.id} onClick={() => this.handleRemove(exp.id)}>
+                                <div>
+                                    <p className="experience-role">{exp.role}</p>
+                                    <p className="experience-company">{exp.company}</p>
+                                    <p className="experience-date">{exp.date}</p>
+                                    <p className="experience-location">{exp.location}</p>
+                                    <p className="experience-details">{exp.details}</p>
+                                </div>
+                            </div>
+                        )
+                    })}
+                    <button 
+                    className="edit-toggle"
+                    type="button"
+                    onClick={this.toggleEdit}
+                    >
+                        Add
+                        </button> 
                 
+                {edit &&
+                <form id="experience-add" className="edit-form">
+                    <label htmlFor="role">Role</label>
+                    <input 
+                    id="role"
+                    type="text"
+                    value={role}
+                    onChange={this.handleRoleChange}
+                    />
+                    <label htmlFor="company">Company</label>
+                    <input
+                    id="company"
+                    type="text"
+                    value={company}
+                    onChange={this.handleCompanyChange}
+                    />
+                    <label htmlFor="start-date">Start Date</label>
+                    <input
+                    id="start-date"
+                    type="text"
+                    value={startDate}
+                    onChange={this.handleStartDateChange}
+                    />
+                    <label htmlFor="end-date">End Date</label>
+                    <input
+                    id="end-date"
+                    type="text"
+                    value={endDate}
+                    onChange={this.handleEndDateChange}
+                    />
+                    <label htmlFor="location">Location</label>
+                    <input
+                    id="location"
+                    type="text"
+                    value={location}
+                    onChange={this.handleLocationChange}
+                    />
+                    <label htmlFor="details">Details</label>
+                    <input
+                    id="details"
+                    type="text"
+                    value={details}
+                    onChange={this.handleDetailsChange}
+                    />
+
+                    <button 
+                    type="button"
+                    onClick={this.onSubmitExperience}
+                    >
+                        Add Experience
+                    </button>
+                    <button
+                        type="button"
+                        onClick={this.toggleEdit}
+                        >
+                            Close
+                        </button>
+                    </form>
+                }
+            </div>
             )
+
+       }
+
+    }
+
+
 
         export default Experience;
