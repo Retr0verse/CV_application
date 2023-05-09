ebd53a455b5b7ae2e467a334c56a251a01c5f165
Jonathan Kennedy <gt4magnet@protonmail.com>
2023-05-09 15:00:36 -0500

    set up both editable parts of intro section intial add

--- cv-project/src/components/title.js
+++ cv-project/src/components/title.js
@@ -1,4 +1,41 @@
 import React, { Component } from 'react';
 
+class Title extends Component {
+    constructor() {
+        super();
 
+        this.state = {
+            name: '',
+            role: '',
+            edit: false
+
+        };
+    }
+
+    handleNameChange = e => {
+        this.setState({
+            name: e.target.value
+        })
+    }
+
+    handleRoleChange = e => {
+        this.setState({
+            role: e.target.value
+        })
+    }
+
+    toggleEdit = e => {
+        this.setState(prevState => ({
+            edit: !prevState.edit
+        }))
+    }
+
+
+    render() {
+const { name, role, edit } = this.state;
+
+
+
+
+   }
 export default Title;
