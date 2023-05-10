a2b3980dc7366488ebcbb2e51919f5ddda41b175
Jonathan Kennedy <gt4magnet@protonmail.com>
2023-05-10 14:48:19 -0500

    finish title section

--- cv-project/src/components/title.js
+++ cv-project/src/components/title.js
@@ -34,8 +34,43 @@ class Title extends Component {
     render() {
 const { name, role, edit } = this.state;
 
+return(
+    <div id="Title">
+        <h1>{name ? name : 'Your name'}</h1>
+        <p className="role">{role ? role : 'Your role'}</p>
+        <button
+            className="edit-toggle"
+            type="button"
+            onClick={this.toggleEdit}
+            >
+                Edit
+            </button>
 
+    {edit && 
+        <form id="intro-edit" className="edit-form">
+            <label htmlfor="name">Your name</label>
+            <input
+                id="name"
+                type="text"
+                onChange={this.handleNameChange}
+                />
+            <label htmlFor="role">Your role</label>
+            <input
+                id="role"
+                type="text"
+                onChange={this.handleRoleChange}
+                />
+                <button
+                    type="button"
+                    onClick={this.toggleEdit}
+                >
+                    Close Edit
+                </button>
+            </form>
+    }
+    </div>
+) 
 
-
+    }
    }
 export default Title;
