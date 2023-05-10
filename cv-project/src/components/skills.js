d1ce50865156f615dbfddf3fb53635ffc0be9593
Jonathan Kennedy <gt4magnet@protonmail.com>
2023-05-10 15:00:23 -0500

    add basic skills section overview

--- cv-project/src/components/skills.js
+++ cv-project/src/components/skills.js
@@ -1,1 +1,45 @@
 import React, { Component } from 'react';
+import uniqid from 'uniqid';
+
+class Skills extends Component {
+    constructor() {
+        super();
+
+        this.state= {
+            skill: '',
+            skills: [
+                {
+                    id: uniqid(),
+                    skill: 'Git'
+                },
+                {
+                    id: uniqid(),
+                    skill: ''
+                },
+                {
+                    id: uniqid(),
+                    skill: ''
+                },
+                {
+                    id: uniqid(),
+                    skill: ''
+                },
+                {
+                    id: uniqid(),
+                    skill: ''
+                },
+                {
+                    id: uniqid(),
+                    skill: ''
+                },
+                {
+                    id: uniqid(),
+                    skill: ''
+                }
+            ],
+            edit: false
+        }
+    }
+
+    
+}
