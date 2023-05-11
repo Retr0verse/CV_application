8740e92413153c1e94b8cb40020843b05a9b3fec
Jonathan Kennedy <gt4magnet@protonmail.com>
2023-05-11 14:37:25 -0500

    remove unneccesary files debug pre css

--- cv-project/src/components/objective.js
+++ cv-project/src/components/objective.js
@@ -30,9 +30,35 @@ render() {
             <h2>Objective</h2>
             <p className="objective">{objective ? objective : 'Analytical-minded and creative team player with a strong background in designing, planning, and maintenance of software programs, desirous of the role of Software Engineer at DC Inc. to hone my technical skills and expertise along with designing creative software and products as per the clients’ needs and enhance the reputation of your organisation.' }</p>
             <button
-        
+                className="edit-toggle"
+                type="button"
+                onClick={this.toggleEdit}
+
+            >
+                Edit
+            </button>
+
+            {edit &&
+                <form id="objective-edit" className="edit-form">
+                    <label htmlFor="objective">Career Objective</label>
+                    <textarea
+                        id="objective"
+                        onClick={this.handleObjectiveChange}
+
+                        />
+                        <button
+                        type="button"
+                        onClick={this.toggleEdit}
+                        >
+                            Close Edit
+                        </button>
+                </form>
+}
+
         </div>
     )
 }
 
 }
+
+export default Objective;
